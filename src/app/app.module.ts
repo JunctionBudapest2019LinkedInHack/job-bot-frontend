f129dedc470283cbdd9522bac23736236d7b2d37
Hoang Phuong Thao <hoangphuong.thao@blindspot.ai>
2019-10-27 08:31:01 +0100

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,32 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
 
+
+const appRoutes: Routes = [
+  { path: 'intro', component: IntroComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '',
+    redirectTo: '/intro',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent],
